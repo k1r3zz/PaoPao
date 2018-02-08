4b948b4f5fa0e16ebcae41c768c06b35d25d409d
jzh <644904088@qq.com>
2018-02-08 15:51:09 +0800

    登录

--- app/src/main/java/com/user/paopao/MenuActivity.java
+++ app/src/main/java/com/user/paopao/MenuActivity.java
@@ -1,14 +1,77 @@
 package com.user.paopao;
 
+import android.annotation.SuppressLint;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RadioGroup;
+import android.widget.TabHost;
 
-public class MenuActivity extends AppCompatActivity {
+import com.user.paopao.entity.LoginEntity;
+import com.user.paopao.utils.ActivityUtil;
+
+public class MenuActivity extends TabActivity {
+
+    //资源文件
+    private Class activitys[] = {LoginEntity.class, LoginEntity.class, LoginEntity.class, LoginEntity.class};//跳转的Activity
+    private String title[] = {"拼团", "询价", "客户", "我的"};//设置菜单的标题
+    private TabHost tabHost;
+    private RadioGroup radioGroup;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ActivityUtil.transplantStatus(MenuActivity.this);
         setContentView(R.layout.activity_main);
+        initView();
+        initTabView();//初始化tab标签
 
+    }
+    private void initView() {
+        tabHost = getTabHost();
+        radioGroup = (RadioGroup) findViewById(R.id.mGroup);
+    }
+
+    private void initTabView() {
+        //创建标签
+        for (int i = 0; i < activitys.length; i++) {
+            //设置跳转activity
+            Intent intent = new Intent(this, activitys[i]);
+
+            //载入view对象并设置跳转的activity
+            TabHost.TabSpec spec = tabHost.newTabSpec(title[i]).setIndicator(title[i]).setContent(intent);
+
+            //添加到选项卡
+            tabHost.addTab(spec);
+        }
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId) {
+                    case R.id.mRbGroupbooking:
+                        tabHost.setCurrentTabByTag(title[0]);
+                        break;
+                    case R.id.mRbEnquiry:
+                        tabHost.setCurrentTabByTag(title[1]);
+                        break;
+                    case R.id.mRbCustomer:
+                        tabHost.setCurrentTabByTag(title[2]);
+                        break;
+                    case R.id.mRbMine:
+                        tabHost.setCurrentTabByTag(title[3]);
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/user/paopao/main/login/LoginActivity.java
+++ app/src/main/java/com/user/paopao/main/login/LoginActivity.java
@@ -1,20 +1,28 @@
 package com.user.paopao.main.login;
 
+import android.widget.LinearLayout;
+
 import com.user.paopao.R;
 import com.user.paopao.base.BaseActivity;
 import com.user.paopao.entity.LoginEntity;
+import com.user.paopao.utils.ActivityUtil;
 import com.user.paopao.utils.NetLog;
 
 public class LoginActivity extends BaseActivity<LoginPresenter, LoginTask> implements LoginContract.View {
+    private LinearLayout llContext;
 
     @Override
     protected void setContentView() {
         setContentView(R.layout.activity_login);
-
+        ActivityUtil.transplantStatus(LoginActivity.this);
     }
 
     @Override
     protected void initViews() {
+        llContext = (LinearLayout) findViewById(R.id.llcontext);
+
+
+        llContext.setPadding(0, ActivityUtil.getStatusBarHeight(LoginActivity.this), 0, 0);
 
     }
 
